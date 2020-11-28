bfe0e27f039e3897131a08d1e7e3fb6fe57e6e5c
Fesseha <885387@cognizant.com>
2020-11-27 23:20:04 -0500

    spring boot commit

---
+++ restspringboot/restspringboot/src/test/java/com/cognizant/restspringboot/RestspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.restspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
